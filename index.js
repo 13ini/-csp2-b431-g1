99fb7279cf8d2089f4d3f9f28e3e62ef45c72432
13ini <binimanandhar1@gmail.com>
2024-09-24 15:42:00 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,61 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
+
+const connectDB = require("./database/db");
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+const authRoutes = require("./routes/authRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+
+/////for auth
+const session = require("express-session");
+const passport = require("passport");
+require("./config/passport");
+// 2. Creating an express app
+const app = express();
+const PORT = 4444;
+// configuration dotenv
+
 const cors = require("cors");
 
-// [SECTION] Routes
-const userRoutes = require("./routes/user");
-const productRoutes = require("./routes/product");
-const cartRoutes = require("./routes/cart");
+app.use(cors());
+// configuration dotenv
 
-
-// Environment Setup
-require('dotenv').config();
-
-// Server setup
-const app = express();
+//connection to db
+connectDB();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+//[SECTION] BAckend Routes
+app.use("/b1/users", userRoutes);
+app.use("/b1/products", productRoutes);
+app.use("/b1/carts", cartRoutes);
+app.use("/b1/orders", orderRoutes);
 
-// Customizing cors options to meet specific requirments
-const corsOptions = {
-	origin: ['http://localhost:8000'], 
-	credentials: true, 
-	optionsSuccessStatus: 200 
-}
-app.use(cors(corsOptions));
+app.use(
+  session({
+    secret: "your_secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-// Database Connection
-mongoose.connect(process.env.MONGODB_STRING,{
-	useUnifiedTopology: true
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+// app.use("/b2/users", authRoutes);
+
+// 3. Defining the port
+// const PORT = process.env.PORT; // 4444;
+
+//  Starting the server
+app.listen(PORT, () => {
+  console.log(`Server-app running on port ${PORT}`);
 });
 
-mongoose.connection.once('open', () => console.log('Now connected to MongoDB Atlas'));
-
-// [SECTION] Backend Routes
-app.use("/users", userRoutes);
-app.use("/product", courseRoutes);
-app.use("/cart", cartRoutes);
-
-
-
-// Server Gateway Response
-if(require.main === module){
-	app.listen(process.env.PORT || 3000, () => {
-		console.log(`API is now online on port ${process.env.PORT || 3000}`);
-	})
-}
-
-module.export = app;
-
+module.exports = app;
